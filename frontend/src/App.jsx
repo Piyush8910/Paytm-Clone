1f376df8230e05eb77b76e6e6d9a4bf3d39b510f
Piyush <agarwalpiyush891@gmail.com>
2024-11-02 23:02:06 +0530

    Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import {HashRouter, Route, Routes} from "react-router-dom"
+import {BrowserRouter, Route, Routes} from "react-router-dom"
 import Signup from "./pages/Signup"
 import Signin from "./pages/Signin"
 import Dashboard from "./pages/Dashboard"
@@ -8,14 +8,14 @@ import SendMoney from "./pages/SendMoney"
 function App() {
 
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/signup" element={<Signup/>}></Route>
         <Route path="/signin" element={<Signin/>}></Route>
         <Route path="/dashboard" element={<Dashboard/>}></Route>
         <Route path="/send" element={<SendMoney/>}></Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   )
 }
 
